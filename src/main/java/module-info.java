490bd7c7551b76a005da849272154c29f13e454a
JaimermXD <jairegra@gmail.com>
2022-06-15 17:27:34 +0200

    Require JavaFX Swing and Java Desktop

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module jaire.serialgeigercounter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
     requires com.fazecast.jSerialComm;
+    requires java.desktop;
 
     opens jaire.serialgeigercounter to javafx.fxml;
     exports jaire.serialgeigercounter;
